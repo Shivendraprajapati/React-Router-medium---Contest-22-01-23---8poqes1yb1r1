65793c1ff937b9e8ac0e8c7ed23d93dcec5deb7d
Shivendraprajapati <111180105+Shivendraprajapati@users.noreply.github.com>
2023-01-22 14:04:33 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,16 +4,15 @@ import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
 import { Calculator } from './Pages/Calculator'
 import { Index } from './Pages/Index'
 import { Routes, Route } from "react-router-dom"
-export const AppRoutes = () =>{
+export const AppRoutes = () => {
     return (
-        
+
         <Routes>
-          
-           <Route path="/" element={<Index />} />
-           <Route path="/bgcolor/:colorname" element={<BackgroundColorChanger />} />
-           <Route path="/calculator" element={<Calculator />} />
-               
-        </Routes>       
+
+            <Route path="/" element={<Index />} />
+            <Route path="/bgcolor/:colorname" element={<BackgroundColorChanger />} />
+            <Route path="/calculator" element={<Calculator />} />
+        </Routes>
 
     )
 }
